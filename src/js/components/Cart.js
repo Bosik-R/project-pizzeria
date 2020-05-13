4a3ce3016232f9a4f2d9d82f9a9b10fab7961451
Tomasz Rosik <Bosik27@gmail.com>
2020-05-13 15:36:43 +0200

    Initialize OOP module form

---
+++ src/js/components/Cart.js
@@ -0,0 +1,137 @@
+import {select, classNames, settings, templates} from '/js/settings.js';
+import utils from '/js/utils.js';
+import CartProduct from './CartProduct.js';
+
+class Cart{
+  constructor(element){
+    const thisCart = this;
+
+    thisCart.products = [];
+    thisCart.deliveryFee = settings.cart.defaultDeliveryFee;
+
+    thisCart.getElements(element);
+    thisCart.initActions();
+
+    console.log('new Cart: ', thisCart);
+  }
+
+  getElements(element){
+    const thisCart = this;
+
+    thisCart.dom = {};
+
+    thisCart.dom.wrapper = element;
+    console.log('thisCart.dom.wrapper:', thisCart.dom.wrapper);
+
+    thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    thisCart.dom.productList = thisCart.dom.wrapper.querySelector(select.cart.productList);
+    thisCart.dom.form = thisCart.dom.wrapper.querySelector(select.cart.form);
+    thisCart.renderTotalKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
+    thisCart.dom.phone = thisCart.dom.wrapper.querySelector(select.cart.phone);
+    thisCart.dom.adress = thisCart.dom.wrapper.querySelector(select.cart.adress);
+
+    for (let key of thisCart.renderTotalKeys){
+      thisCart.dom[key] = thisCart.dom.wrapper.querySelectorAll(select.cart[key]);
+    }
+  }
+
+  initActions(){
+    const thisCart = this;
+
+    thisCart.dom.toggleTrigger.addEventListener('click', function(){
+      thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+    });
+
+    thisCart.dom.productList.addEventListener('updated', function(){
+      thisCart.update();
+    });
+
+    thisCart.dom.productList.addEventListener('remove', function(){
+      thisCart.remove(event.detail.cartProduct);
+    });
+
+    thisCart.dom.form.addEventListener('submit', function(){
+      event.preventDefault();
+      thisCart.sendOrder();
+    });
+  }
+
+  remove(cartProduct){
+    const thisCart = this;
+
+    const index = thisCart.products.indexOf(cartProduct);
+    thisCart.products.splice(index, 1);
+
+    cartProduct.dom.wrapper.remove();
+
+    thisCart.update();
+  }
+
+  add(menuProduct){
+    const thisCart = this;
+
+    const generateHTML = templates.cartProduct(menuProduct);
+    const generateDOM = utils.createDOMFromHTML(generateHTML);
+
+    thisCart.dom.productList.appendChild(generateDOM);
+    thisCart.products.push(new CartProduct(menuProduct, generateDOM));
+
+    thisCart.update();
+  }
+
+  update(){
+    const thisCart = this;
+
+    thisCart.totalNumber = 0;
+    thisCart.subtotalPrice = 0;
+
+    for (let product  of thisCart.products){
+      thisCart.subtotalPrice += product.price;
+      thisCart.totalNumber += product.amount;
+    }
+
+    thisCart.totalPrice = thisCart.subtotalPrice + thisCart.deliveryFee;
+
+    for (let key of thisCart.renderTotalKeys){
+      for (let elem of thisCart.dom[key]){
+        elem.innerHTML = thisCart[key];
+      }
+    }
+  }
+
+  sendOrder(){
+    const thisCart = this;
+
+    const url = settings.db.url + '/' + settings.db.order;
+
+    const payload = {
+      totalNumber: thisCart.totalNumber,
+      subtotalPrice: thisCart.subtotalPrice,
+      deliveryFee: thisCart.deliveryFee,
+      totalPrice: thisCart.totalPrice,
+      products: [],
+    };
+
+    for (let product of thisCart.products){
+      product.getData();
+      payload.products.push(product.payload);
+    }
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+    fetch(url, options)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse: ', parsedResponse);
+      });
+  }
+}
+
+
+export default Cart;
